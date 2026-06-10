724ca2f8566ba5906910dc42d6712e3942ebf7d7
basantkhira <basant.khira@gmail.com>
2026-06-10 22:10:52 +0300

    debruijn graph

---
+++ II/debruijn.py
@@ -0,0 +1,28 @@
+def main():
+    k = int(input("k: "))
+    text = input("Text: ")
+    
+    graph = de_bruijn(k, text)
+    
+    with open("output3.txt", "w") as f:
+        for node, neighbors in sorted(graph.items()):
+            line = f"{node} : {' '.join(neighbors)}"
+            f.write(line + "\n")
+            
+            
+def de_bruijn(k, text):
+    edges = {}
+    
+    for i in range(len(text) - k + 1):
+        kmer = text[i:i+k]
+        prefix = kmer[:-1]   # first k-1 characters
+        suffix = kmer[1:]    # last k-1 characters
+        
+        if prefix not in edges:
+            edges[prefix] = []
+        edges[prefix].append(suffix)
+    
+    return edges
+
+if __name__ == "__main__":
+    main()
